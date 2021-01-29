630d351b6ec47129443408a0fbaa28ac6ccb41dc
Ameed Sayeh <ameed.iyad.sayeh@gmail.com>
2021-01-29 22:34:47 +0200

    fetch items and display details

--- lib/screens/cart_screen/cart_items_list.dart
+++ lib/screens/cart_screen/cart_items_list.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:mock_store/models/cart.dart';
+import 'package:mock_store/models/store_item.dart';
 import 'package:mock_store/services/store_service.dart';
 
 class CartItemsList extends StatefulWidget {
@@ -27,10 +29,48 @@ class _CartItemsListState extends State<CartItemsList> {
     return Expanded(
       child: ListView.builder(
         itemCount: _cart != null ? _cart.products.length : 0,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(_cart.products[index].productId.toString()),
+        itemBuilder: (context, index) => FutureBuilder(
+          future: _fetchItem(_cart.products[index].productId),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.hasData) {
+              final item = snapshot.data as StoreItem;
+
+              return ListTile(
+                contentPadding: EdgeInsets.all(16.0),
+                leading: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: CachedNetworkImage(
+                    imageUrl: item.imageURL,
+                    placeholder: (context, url) => Text("loading image"),
+                    fadeInDuration: Duration(),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                title: Text(
+                  item.title,
+                  maxLines: 2,
+                ),
+                subtitle: Text(
+                  item.description,
+                  maxLines: 4,
+                ),
+              );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return LinearProgressIndicator();
+            } else {
+              return ListTile(
+                title: Text("Cannot Fetch Item"),
+              );
+            }
+          },
         ),
       ),
     );
+  }
+
+  _fetchItem(productID) async {
+    return StoreService().getItem(productID);
   }
 }
