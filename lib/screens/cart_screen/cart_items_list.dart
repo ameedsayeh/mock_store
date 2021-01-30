294c2d894577380bda9792ddadedb466d1fb799a
Ameed Sayeh <ameed.iyad.sayeh@gmail.com>
2021-01-30 21:03:42 +0200

    refactor cart fetch to future builder

--- lib/screens/cart_screen/cart_items_list.dart
+++ lib/screens/cart_screen/cart_items_list.dart
@@ -12,73 +12,114 @@ class CartItemsList extends StatefulWidget {
 class _CartItemsListState extends State<CartItemsList> {
   Cart _cart;
 
-  @override
-  void initState() {
-    super.initState();
-
-    _fetchCart();
-  }
-
-  _fetchCart() async {
-    _cart = await StoreService().getCartItems();
-    setState(() {});
+  Future<Cart> _fetchCart() {
+    return StoreService().getCartItems();
   }
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ListView.builder(
-        itemCount: _cart != null ? _cart.products.length : 0,
-        itemBuilder: (context, index) => FutureBuilder(
-          future: _fetchItem(_cart.products[index].productId),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done &&
-                snapshot.hasData) {
-              final item = snapshot.data as StoreItem;
+      child: FutureBuilder(
+        future: _fetchCart(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return buildLoadingIndicator();
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            _cart = snapshot.data;
 
-              return ListTile(
-                contentPadding: EdgeInsets.all(16.0),
-                leading: SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: CachedNetworkImage(
-                    imageUrl: item.imageURL,
-                    placeholder: (context, url) => Text("loading image"),
-                    fadeInDuration: Duration(),
-                    fit: BoxFit.contain,
+            return buildListView();
+          } else {
+            return buildTryAgain();
+          }
+        },
+      ),
+    );
+  }
+
+  Widget buildLoadingIndicator() {
+    return Center(
+      child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation(Colors.black87),
+      ),
+    );
+  }
+
+  Widget buildTryAgain() {
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.report_problem,
+            size: 48,
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Some error happened, please try again"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildListView() {
+    return ListView.builder(
+      itemCount: _cart != null ? _cart.products.length : 0,
+      itemBuilder: (context, index) => FutureBuilder(
+        future: _fetchItem(_cart.products[index].productId),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.hasData) {
+            final item = snapshot.data as StoreItem;
+
+            return ListTile(
+              contentPadding: EdgeInsets.all(16.0),
+              leading: SizedBox(
+                width: 100,
+                height: 100,
+                child: CachedNetworkImage(
+                  imageUrl: item.imageURL,
+                  placeholder: (context, url) => Text("loading image"),
+                  fadeInDuration: Duration(),
+                  fit: BoxFit.contain,
+                ),
+              ),
+              title: Text(
+                item.title,
+                maxLines: 2,
+              ),
+              subtitle: Text(
+                item.description,
+                maxLines: 4,
+              ),
+              trailing: Column(
+                children: [
+                  Text(
+                    "X ${_cart.products[index].quantity}",
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
-                ),
-                title: Text(
-                  item.title,
-                  maxLines: 2,
-                ),
-                subtitle: Text(
-                  item.description,
-                  maxLines: 4,
-                ),
-                trailing: Column(
-                  children: [
-                    Text(
-                      "X ${_cart.products[index].quantity}",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Spacer(),
-                    Text(
-                      "${item.price} \$",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              );
-            } else if (snapshot.connectionState == ConnectionState.waiting) {
-              return LinearProgressIndicator();
-            } else {
-              return ListTile(
-                title: Text("Cannot Fetch Item"),
-              );
-            }
-          },
-        ),
+                  Spacer(),
+                  Text(
+                    "${item.price} \$",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return LinearProgressIndicator();
+          } else {
+            return ListTile(
+              title: Text("Cannot Fetch Item"),
+            );
+          }
+        },
       ),
     );
   }
